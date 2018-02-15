54812185ed2c1ed8661904b4b9bcbbfe7a620bcc
Arkadiusz Wilczek <arekwil@gmail.com>
2018-02-15 20:41:14 +0100

    JSON parse

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,67 @@
 package com.udacity.sandwichclub.utils;
 
+
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//{"name":
+//           {
+//            "mainName" :"Ham and cheese sandwich",
+//            "alsoKnownAs":[]
+//            },
+// "placeOfOrigin":"",
+// "description":"A ham and cheese sandwich",
+// "image":"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Grilled_ham_and_cheese_014.JPG/800px-Grilled_ham_and_cheese_014.JPG",
+// "ingredients":["Sliced bread","Cheese","Ham"]}
+
+
 public class JsonUtils {
+    private static final String NAME = "name";
+    private static final String MAIN_NAME = "mainName";
+    private static final String ALSO_KNOWN_AS = "alsoKnownAs";
+    private static final String PLACE_OF_ORIGIN = "placeOfOrigin";
+    private static final String DESCRIPTION = "description";
+    private static final String IMAGE = "image";
+    private static final String INGREDIENTS = "ingredients";
+
 
     public static Sandwich parseSandwichJson(String json) {
+        try {
+            JSONObject fullJsonSandwich = new JSONObject(json);
+
+            JSONObject name = fullJsonSandwich.getJSONObject(NAME);
+            String mainName = name.getString(MAIN_NAME);
+            JSONArray alsoKnownAsJson = name.getJSONArray(ALSO_KNOWN_AS);
+
+            List<String> alsoKnownAs = new ArrayList<>();
+
+            for ( int i=0;i<alsoKnownAsJson.length();i++){
+                alsoKnownAs.add( alsoKnownAsJson.getString(i) );
+            }
+
+            String placeOfOrigin = fullJsonSandwich.getString(PLACE_OF_ORIGIN);
+            String description = fullJsonSandwich.getString(DESCRIPTION);
+            String image = fullJsonSandwich.getString(IMAGE);
+            JSONArray ingredientsJson = fullJsonSandwich.getJSONArray(INGREDIENTS);
+
+            List<String> ingredients = new ArrayList<>();
+
+            for ( int i=0;i<ingredientsJson.length();i++){
+                ingredients.add( ingredientsJson.getString(i) );
+            }
+
+            return new Sandwich(mainName,alsoKnownAs,placeOfOrigin,description,image,ingredients);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
